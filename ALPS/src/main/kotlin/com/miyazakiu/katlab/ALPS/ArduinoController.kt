8ec4ca73c8e8319094bbad7e2254651ff2463cde
Tatsumi0000 <ti18027@student.miyazaki-u.ac.jp>
2019-09-02 01:55:24 +0900

    Add: レスポンス確認用のメソッドとjavadocを生成するためのコメント

--- ALPS/src/main/kotlin/com/miyazakiu/katlab/ALPS/ArduinoController.kt
+++ ALPS/src/main/kotlin/com/miyazakiu/katlab/ALPS/ArduinoController.kt
@@ -4,35 +4,41 @@ import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.*
 
+/**
+ * このクラスは，Arduino言語を解析した結果をいい感じにするクラスです．
+ *
+ */
 @Controller
 @ResponseBody
 @RequestMapping("api/arduino")
 class ArduinoController {
 
-    @RequestMapping(value = ["hello"], method = [RequestMethod.GET])
+    /**
+     * サーバが生きているかを返すだけのメソッド
+     * <b>api/arduino/response</b>にアクセスしてね．
+     * @param response 確認用のパラメータ
+     * @return 引数で受取った値をjson形式にして返す
+     */
+    @RequestMapping(value = ["checkResponse/{response}"], method = [RequestMethod.GET])
     @ResponseBody
-    fun hello_world(@RequestBody sourceCode: String): String {
-        val json = """[{"aaa": ${sourceCode}}]"""
+    fun checkResponse(@PathVariable response: String): String {
+        val json = """[{"received": $response}]"""
+        print(json)
         return json
     }
 
-//    @RequestMapping(value = ["/hello/{name}"], method = [RequestMethod.GET])
-//    fun hello_name(@PathVariable name: String, model: Model): String {
-//        model.addAttribute("name", name)
-//        return "arduino" // Thymeleaf テンプレートファイル名
-//    }
-
+    /**
+     * POST通信で受取ったソースコードをjson形式にして返すメソッド
+     * <b>api/arduino/parse</b>のbodyのsourceCodeにPOSTしてアクセスしてね．
+     * @param sourceCode 解析したいソースコード
+     * @return ADVISで解析するためのjson
+     */
     @RequestMapping(value = ["parse"], method = [RequestMethod.POST])
     @ResponseBody
-    fun ArduinoParse(@RequestBody sourceCode: String): String {
+    fun arduinoParse(@RequestBody sourceCode: String): String {
         val json = """[{"aaa": ${sourceCode}}]"""
         println(json)
         return json
     }
-
-    @PostMapping
-    fun arduinoParse(sourceCode: String): String{
-    return ""
-}
 
 }
